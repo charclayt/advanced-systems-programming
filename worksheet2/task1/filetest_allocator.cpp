010688f8883def6914a4fe85fc1783ea3eeda545
Charlie Clayton <charclayt@gmail.com>
2024-10-23 19:17:55 +0100

    task1: update test file

--- worksheet2/task1/filetest_allocator.cpp
+++ worksheet2/task1/filetest_allocator.cpp
@@ -1,52 +1,42 @@
 #include "bump_allocator.hpp"
 
 int main() {
-    task1::bump_allocator a(40);
+    // Create bump_allocator object with size 40
+    task1::bump_allocator allocator(40);
 
-    char* b = a.allocate<char>(1);
+    // Allocate space for one char
+    char* test_char = allocator.allocate<char>(1);
 
-    int* x = a.allocate<int>(1);
+    // Allocate space for one int
+    int* test_int = allocator.allocate<int>(1);
 
-    char* y = a.allocate<char>(1);
+    // Initialise int pointer to 2
+    *test_int = 2;
 
-    char* z = a.allocate<char>(3);
+    // Print the value of the int pointer if allocation successful
+    if (test_int != nullptr) {
+        std::cout << *test_int << std::endl;
+    } else {
+        std::cout << "nullptr" << std::endl;
+    }
 
-    int* w = a.allocate<int>(1);
+    // Allocate space for another char
+    char* test_char2 = allocator.allocate<char>(1);
 
-    // // char* ptr3 = a.allocate<char>(1);
+    // Initialise int pointer to 'a'
+    *test_char2 = 'a';
 
-    // // int* ptr = a.allocate<int>(1);
+    // Print the value of the char pointer if allocation successful
+    if (test_char2 != nullptr) {
+        std::cout << *test_char2 << std::endl;
+    } else {
+        std::cout << "nullptr" << std::endl;
+    }
 
-    // // *ptr = 2;
-
-    // // if (ptr != nullptr) {
-    // //     std::cout << *ptr << std::endl;
-    // // } else {
-    // //     std::cout << "nullptr" << std::endl;
-    // // }
-
-    // // char* ptr2 = a.allocate<char>(1);
-
-    // // *ptr2 = 'a';
-
-    // int* ptr2 = a.allocate<int>(1);
-
-    // if (ptr2 != nullptr) {
-    //     std::cout << *ptr2 << std::endl;
-    // } else {
-    //     std::cout << "nullptr" << std::endl;
-    // }
-
-    // a.deallocate();
-
-    // int* ptr3 = a.allocate<int>(1);
-    // // *ptr3 = 3;
-    
-    // if (ptr3 != nullptr) {
-    //     std::cout << *ptr3 << std::endl;
-    // } else {
-    //     std::cout << "nullptr" << std::endl;
-    // }
+    // Deallocate all allocations, should print "Allocation counter = 0, deallocating all memory" and reset pointer to start
+    allocator.deallocate();
+    allocator.deallocate();
+    allocator.deallocate();
 
     return 0;
 }
